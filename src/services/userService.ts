2907a92bdd4e8c8d000dd4d1c5837599f6ba4ce1
KennethMinn <minthukha2472005@gmail.com>
2024-07-05 19:35:05 +0630

    fix : updated user response fields

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,6 +4,10 @@ import User from "../models/user";
 export class UserService {
   async getAllUsers(req: Request, res: Response) {
     const users = await User.find();
-    return res.status(200).json({ users });
+    const userResponseArr = users.map((user) => ({
+      id: user.id,
+      email: user.email,
+    }));
+    return res.status(200).json({ users: userResponseArr });
   }
 }
